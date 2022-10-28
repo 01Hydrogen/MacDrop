aa068bc9344ef59f5c348c69cbb9760cc19797f8
Kai Sun <bryant.kai1227@gmail.com>
2022-10-28 17:26:06 -0400

    add Cart Intialization

---
+++ Cart/src/main/java/com/LuckyStar/Cart/business/CartRegistry.java
@@ -0,0 +1,22 @@
+package com.LuckyStar.Cart.business;
+
+import com.LuckyStar.Cart.business.entities.Cart;
+import com.LuckyStar.Cart.ports.CartFinder;
+import com.LuckyStar.Cart.ports.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CartRegistry implements CartFinder {
+    private final CartRepository repository;
+
+    @Autowired
+    public CartRegistry(CartRepository repository){ this.repository = repository;}
+
+    @Override
+    public List<Cart> findAll() {
+        return repository.findAll();
+    }
+}
---
+++ Cart/src/main/resources/schema.sql
@@ -0,0 +1,3 @@
+INSERT INTO Cart
+(id, user_id,menu_id,amount)
+VALUES('bibi','shit','fkfk', 1);
